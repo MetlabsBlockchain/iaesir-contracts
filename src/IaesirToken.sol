c0a70ea2209b9670c51c345f39750e956d6cafd1
CryptoGhost277 <cryptoghost277@gmail.com>
2025-03-15 13:09:59 +0100

    create token contract

---
+++ src/IaesirToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import '../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
+
+contract IaesirToken is ERC20 {
+  uint256 _totalSupply = 1000000000 * 1e18;
+
+  constructor(address distributorAddress_) ERC20('IAESIR', 'IASR') {
+    _mint(distributorAddress_, _totalSupply);
+  }
+}
