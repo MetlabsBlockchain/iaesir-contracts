809a265cb920f21439105e38d0bea0c452919a2a
CryptoGhost277 <cryptoghost277@gmail.com>
2025-03-26 19:49:39 +0100

    add code system

--- src/IaesirPresale.sol
+++ src/IaesirPresale.sol
@@ -26,6 +26,7 @@ contract IaesirPresale is ReentrancyGuard, Pausable, Ownable {
     uint256 public usdPhase1;
     uint256 public tokensSoldPhase0;
     uint256 public tokensSoldPhase1;
+    uint256 public thresholdToReferral;
     address public paymentToken;
     address public paymentWallet;
     uint256[][3] public phases;
@@ -35,14 +36,16 @@ contract IaesirPresale is ReentrancyGuard, Pausable, Ownable {
     mapping(uint256 => User) public userPhase0;
     mapping(uint256 => User) public userPhase1;
     mapping(address => bool) public isWhitelisted;
+    mapping(address => bytes) public referralCode;
 
     event TokensBought(address indexed user, uint256 indexed tokensBought, uint256 usdRaised, uint256 timestamp);
 
-    constructor(uint256[][3] memory phases_, address paymentToken_, address paymentWallet_, address aggregatorContract_) Ownable(paymentWallet_) {
+    constructor(uint256[][3] memory phases_, address paymentToken_, address paymentWallet_, address aggregatorContract_, uint256 thresholdToReferral_) Ownable(paymentWallet_) {
         paymentToken = paymentToken_;
         paymentWallet = paymentWallet_;
         aggregatorContract = IAggregator(aggregatorContract_);
         phases = phases_;
+        thresholdToReferral_ = thresholdToReferral;
     }
 
     function checkIfEnoughTokens(uint256 tokensToReceive) internal view {
@@ -137,11 +140,31 @@ contract IaesirPresale is ReentrancyGuard, Pausable, Ownable {
         emit TokensBought(msg.sender, tokenAmountToReceive, usdAmount, block.timestamp);
     }
 
+    function generateReferralCode() public {
+        require(referralCode[msg.sender].length != 0, "Already generated code");
+
+        // Check if user has invested more than $250
+        uint256 positionPhase0 = userPositionPhase0[msg.sender];
+        User memory userDataPhase0 = userPhase0[positionPhase0];
+        uint256 amountInPhase0 = userDataPhase0.amount * phases[0][1];
+        uint256 positionPhase1 = userPositionPhase1[msg.sender];
+        User memory userDataPhase1 = userPhase0[positionPhase1];
+        uint256 amountInPhase1 = userDataPhase1.amount * phases[1][1];
+        require(amountInPhase0 + amountInPhase1 >= thresholdToReferral, "Not invested minimum amount");
+
+        bytes memory code = abi.encodePacked(msg.sender, block.number);
+        referralCode[msg.sender] = code;
+    }
+
     function getLatestPrice() public view returns (uint256) {
         (, int256 price, , uint256 updatedAt, ) = aggregatorContract.latestRoundData();
         if (updatedAt < block.timestamp - 2 hours) revert("Chainlink data is too old");
         price = (price * (10 ** 10));
         return uint256(price);
+    }
+
+    function setThresholdToReferral(uint256 newThreshold_) public onlyOwner() {
+        thresholdToReferral = newThreshold_;
     }
 
     function pausePresale() public onlyOwner {
