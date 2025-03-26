b83c86ccf59e4219d04d42d2ea1a73e92dbd9c00
CryptoGhost277 <cryptoghost277@gmail.com>
2025-03-26 19:56:21 +0100

    deploy script

--- script/DeployIaesirPresale.s.sol
+++ script/DeployIaesirPresale.s.sol
@@ -16,11 +16,12 @@ contract DeployIaesirPresale is Script {
         address usdtAddress_ = 0x55d398326f99059fF775485246999027B3197955; // USDT BSC
         address aggregatorContract_ = 0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE; // PriceFeed BNB/USD en BSC
         address paymentWallet_ = 0x56E4CF839281f06c6B25a2037C5797C40D35fF2c; 
+        uint256 referralThreshold = 250 * 1e18;
 
         phases_[0] = [110_000_000  * 10**18, 40000, 47128000]; // @audit CAMBIAR A FINAL
         phases_[1] = [200_000_00  * 10**18, 5000, 47188000]; // @audit CAMBIAR A FINAL
 
-        IaesirPresale presale = new IaesirPresale(phases_, usdtAddress_, paymentWallet_, aggregatorContract_);
+        IaesirPresale presale = new IaesirPresale(phases_, usdtAddress_, paymentWallet_, aggregatorContract_, referralThreshold);
 
         vm.stopBroadcast();
         return presale;
